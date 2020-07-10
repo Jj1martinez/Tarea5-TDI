15aa25bc4ac61d2812d05b3cecf28792238a3239
Jj1martinez <juan@MacBook-Pro-de-Juan.local>
2020-07-10 12:15:29 -0400

    listo

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,125 +3,180 @@ const pkg = require('../../package.json');
 const axios = require('axios');
 
 const router = new KoaRouter();
-const url = 'https://integracion-rick-morty-api.herokuapp.com/';
+const url = 'https://integracion-rick-morty-api.herokuapp.com/graphql';
+const query_episodes = (number)=>{
+  return `
+  query{
+    episodes(page: ${number}) {
+        info {
+          count
+          pages
+        }
+        results {
+          name
+          id
+          episode
+          air_date
+        }
+      }
+    }`
+}
+const query_characters = (number)=>{
+  return `
+  query{
+    characters(page: ${number}) {
+        info {
+          count
+          pages
+        }
+        results {
+          name
+          id
+        }
+      }
+    }`
+}
+const query_locations = (number)=>{
+  return `
+  query{
+    locations(page: ${number}) {
+        info {
+          count
+          pages
+        }
+        results {
+          name
+          id
+        }
+      }
+    }`
+}
+const query_episode = (number)=>{
+  return `
+  query{
+    episode(id: ${number}) {
+      id
+      name
+      air_date
+      episode
+      characters{
+        id
+        name
+      }
+      created
+      }
+    }`
+}
+
+const query_character = (number)=>{
+  return `
+  query{
+    character(id: ${number}) {
+      id
+      name
+      status
+      species
+      type
+      gender
+      origin{
+        id
+        name
+      }
+      location{
+        id
+        name
+      }
+      episode{
+        id
+        name
+      }
+      image
+      created
+    }
+  }`
+}
+const query_location = (number)=>{
+  return `
+  query{
+    location(id: ${number}) {
+      id
+      name
+      type
+      residents{
+        id
+        name
+      }
+      dimension
+      created
+    }
+  }`
+}
+async function get_all_episodes() {
+  const response = await axios.post(url, {
+    query: query_episodes(1)
+  })
+  const response2 = await axios.post(url, {
+    query: query_episodes(2)
+  })
+  const results = response.data.data.episodes.results.concat(response2.data.data.episodes.results)
+  return results
+}
 
 
 router.get('/', async (ctx) => {
-  console.log(ctx)
-  var response = await axios.get(url+'api/episode')
-  if ((response.data.info.pages)>1) {
-    for (let index = 1; index < response.data.info.pages; index++) {
-      let res = await axios.get(url+'api/episode?page='+(index+1).toString())
-      res.data.results.forEach(element => {
-            response.data.results.push(element);
-      });
-    }
-  }
-  console.log(response.data);
-  await ctx.render('index', { data: response.data,
+  const results = await get_all_episodes()
+  await ctx.render('index', { data: results,
     profileEpisodePath: episode => ctx.router.url('episode.profile', { id: episode.id })});
 });
 
 router.get('episode.profile', 'episode/:id', async (ctx) => {
-  var response = await axios.get(url+'api/episode/'+ctx.params.id)
-  var characters = [];
-  const n_charc= response.data.characters.length;
-  // console.log(n_charc);
-  for (let index = 0; index < n_charc; index++) {
-    console.log(response.data.characters[index]);
-    let matches = response.data.characters[index].toString().match(/(\d+)/);
-    // console.log(matches[0]);
-    characters.push(matches[0]);
-  }
-  // console.log(characters);
-  var response2 = await axios.get(url+'api/character/'+characters.join(','));
-  // console.log(response2.data);
-  await ctx.render('episode',{data: response.data, characters:response2.data});
+  const response = await axios.post(url, {
+    query: query_episode(ctx.params.id)
+  })
+  await ctx.render('episode',{data: response.data.data.episode,
+    characters:response.data.data.episode.characters });
 });
 
 
 router.get('character.profile', 'character/:id', async (ctx) => {
-  var response = await axios.get(url+'api/character/'+ctx.params.id)
-  console.log(response.data);
-  // Buscar Episodios
-  var episodes = [];
-  var episode_request = await axios.get(url+'api/episode')
-  if ((episode_request.data.info.pages)>1) {
-    for (let index = 1; index < episode_request.data.info.pages; index++) {
-      let res = await axios.get(url+'api/episode?page='+(index+1).toString())
-      res.data.results.forEach(element => {
-        episode_request.data.results.push(element);
-      });
-    }  
-  }
-  // Tengo todos los episodios, buscamos el que coincida
-  episode_request.data.results.forEach(element => {
-    if (element.characters.indexOf(url+'api/character/'+ctx.params.id) > -1) {
-      episodes.push(element.id.toString())
-      // console.log(element.id.toString());
-    }
-  });
-  // HAcemos la consulta 
-  var response2 = await axios.get(url+'api/episode/'+episodes.join(','));
-  //
-  console.log(response2.data.length);
-  // buscamos id de lugares
-  let id_origin;
-  if (response.data.origin.name!='unknown') {
-    id_origin = response.data.origin.url.toString().match(/(\d+)/);
-  }else{
-    id_origin = '0';
-  }
-  console.log(response.data)
-  let id_location = response.data.location.url.toString().match(/(\d+)/);
-  console.log(id_origin);
-  // 
-
-  if (typeof response2.data.length !== "undefined") {
-    // console.log(response2.data[0].name);
-    await ctx.render('character',{data: response.data, episodes: response2.data,
-    origin: id_origin, location: id_location})
-  } else {
-    await ctx.render('character',{data: response.data, episodes: [response2.data],
-    origin: id_origin, location: id_location})
-  }
+  const response = await axios.post(url, {
+    query: query_character(ctx.params.id)
+  })
+  const dic = {data: response.data.data.character, episodes: response.data.data.character.episode,
+    origin: response.data.data.character.origin, location: response.data.data.character.location}
+  await ctx.render('character',dic)
 });
 
 router.get('location.profile', 'place/:id', async (ctx) => {
-  var response = await axios.get(url+'api/location/'+ctx.params.id)
-  var characters = [];
-  const n_charc= response.data.residents.length;
-  console.log(response.data);
-  for (let index = 0; index < n_charc; index++) {
-    // console.log(response.data.residents[index]);
-    let matches = response.data.residents[index].toString().match(/(\d+)/);
-    // console.log(matches[0]);
-    characters.push(matches[0]);
-  }
-  // console.log(characters);
-  var response2 = await axios.get(url+'api/character/'+characters.join(','));
-  if (typeof response2.data.length !== "undefined") {
-    await ctx.render('place',{data: response.data, characters:response2.data})
-  }
-  else{
-    await ctx.render('place',{data: response.data, characters:[response2.data]})
-  }
+  const response = await axios.post(url, {
+    query: query_location(ctx.params.id)
+  })
+
+  await ctx.render('place',{data: response.data.data.location, 
+    characters:response.data.data.location.residents})
+  
 });
 
 
 router.get('searchresults', 'search', async (ctx) => {
-  console.log(ctx.query.name);
   var requests = [];
+
   for (let index = 0; index < 2; index++) {
-    const request = axios.get(url+'api/episode?page='+(index+1).toString());
+    const request = axios.post(url, {
+      query: query_episodes(index+1)
+    })
     requests.push(request);
   }
   for (let index = 0; index < 25; index++) {
-    const request = axios.get(url+'api/character?page='+(index+1).toString());
+    const request= axios.post(url, {
+      query: query_characters(index+1)
+    })
     requests.push(request);
   }
   for (let index = 0; index < 4; index++) {
-    const request = axios.get(url+'api/location?page='+(index+1).toString());
+    const request = axios.post(url, {
+      query: query_locations(index+1)
+    })
     requests.push(request);
   }
   var episodes_data_f=[];
@@ -135,19 +190,14 @@ router.get('searchresults', 'search', async (ctx) => {
       var location_data=[];
       
       for (let index = 0; index < 2; index++) {
-        episodes_data=episodes_data.concat(responses[index].data.results)    
+        episodes_data=episodes_data.concat(responses[index].data.data.episodes.results)    
       }
       for (let index = 2; index < 27; index++) {
-        character_data=character_data.concat(responses[index].data.results) 
+        character_data=character_data.concat(responses[index].data.data.characters.results) 
       }
       for (let index = 27; index < 31; index++) {
-        location_data=location_data.concat(responses[index].data.results) 
+        location_data=location_data.concat(responses[index].data.data.locations.results) 
       }
-
-      // console.log(episodes_data[1]);
-      // console.log(character_data[2]);
-      // console.log(location_data[2]);
-      // console.log(responseTwo.data);
       episodes_data.forEach(element=> {
         if (element.name.toUpperCase().indexOf(ctx.query.name.toUpperCase())>-1){
           episodes_data_f.push([element.name,element.id]);
@@ -165,15 +215,13 @@ router.get('searchresults', 'search', async (ctx) => {
           location_data_f.push([element.name,element.id]);
           counter++;
         }
-      
       })
-      console.log(episodes_data_f);
-      ctx.render('search',{episodes:episodes_data_f,characters:character_data_f,
-        locations:location_data_f}) 
+      const dic = {episodes:episodes_data_f,characters:character_data_f,
+        locations:location_data_f,counter: counter}
+      return dic
     })
   );
-  await ctx.render('search',{episodes:episodes_data_f,characters:character_data_f,
-    locations:location_data_f, counter:counter}) 
+  await ctx.render('search',promesas) 
 
 
 });
